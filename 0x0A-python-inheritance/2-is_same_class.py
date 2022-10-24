4ef8655eff6558fa3290973f2f395b054e9716bb
raheemamer <raheem.amer22@gmail.com>
2022-10-24 18:09:14 +0200

    Adding tasks

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains a single function"""
+
+
+def is_same_class(obj, a_class):
+    """
+        Check if an object is an instance of the specified class
+    """
+
+    return (type(obj) == a_class)
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Contains a `BaseGeometry` class"""
+
+
+class BaseGeometry():
+    """An empty class"""
+    pass
+
+    def area(self):
+        """Calculate the area of the geometry"""
+        raise Exception("area() is not implemented")
